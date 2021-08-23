b6eefc256a09509f9abc1092b15ddb87b5b42d8b
Poenaru Dan <dantpoenaru@gmail.com>
2021-08-23 20:13:44 +0300

    Add the js to hide azurite ids if only dept are required.

--- js/asurite-id-field.js
+++ js/asurite-id-field.js
@@ -3,6 +3,14 @@
 
   Drupal.behaviors.asuriteIdField = {
     attach: function (context, settings) {
+
+      // Check when to display the widget. It is not removed in case we go back
+      // to having this option.
+      check_azurite_widget_display();
+      $('select[name="settings[block_form][field_isearchtype]"]').change(function() {
+        check_azurite_widget_display();
+      });
+
       // Check if there are directory type fields.
       if ($(context).find('.asurite-tree').length) {
         $('.asurite-tree').each(function(index) {
@@ -86,7 +94,17 @@ function create_tree(default_values) {
       "plugins" : [ "checkbox", "types" ]
     });
   });
- 
+
+}
+
+function check_azurite_widget_display() {
+  if ($("select[name='settings[block_form][field_isearchtype]']").val() === 'depList') {
+    $('.field--widget-field-asurite-id-widget').hide();
+  }
+  else {
+    $('.field--widget-field-asurite-id-widget').show();
+  }
+
 }
 
 })(jQuery, Drupal, drupalSettings);
